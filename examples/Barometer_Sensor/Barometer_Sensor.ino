d26bc1a48531a3b76e0e6ef8b57aa7bc08adefd3
Jack Shao <jacky.shaoxg@gmail.com>
2014-08-01 10:53:05 +0800

    updated the README file about the copyright

--- examples/Barometer_Sensor/Barometer_Sensor.ino
+++ examples/Barometer_Sensor/Barometer_Sensor.ino
@@ -1,10 +1,33 @@
-/* Barometer demo V1.0
-* Based largely on code by  Jim Lindblom
-* Get pressure, altitude, and temperature from the BMP085.
-* Serial.print it out at 9600 baud to serial monitor.
-*
-* By:http://www.seeedstudio.com
-*/
+/*
+ * Barometer_Sensor.ino
+ * Example sketch for barometer
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Jim Lindblom, LG
+ * Create Time:
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include "Barometer.h"
 #include <Wire.h>
 float temperature;
@@ -15,16 +38,16 @@ Barometer myBarometer;
 void setup(){
   Serial.begin(9600);
   myBarometer.init();
-  
+
 }
 
 void loop()
 {
    temperature = myBarometer.bmp085GetTemperature(myBarometer.bmp085ReadUT()); //Get the temperature, bmp085ReadUT MUST be called first
    pressure = myBarometer.bmp085GetPressure(myBarometer.bmp085ReadUP());//Get the temperature
-   altitude = myBarometer.calcAltitude(pressure); //Uncompensated caculation - in Meters 
-   atm = pressure / 101325; 
-  
+   altitude = myBarometer.calcAltitude(pressure); //Uncompensated caculation - in Meters
+   atm = pressure / 101325;
+
   Serial.print("Temperature: ");
   Serial.print(temperature, 2); //display 2 decimal places
   Serial.println("deg C");
